76521ef4295141c321981a5c68f1c4328148b23a
z <z@pingflag.com>
2022-04-30 15:32:18 +1000

    added label colour to card background

--- trello-experiment.js
+++ trello-experiment.js
@@ -7,8 +7,13 @@ listOfCards.forEach(card => {
     listCardLabels = card.querySelector(".list-card-labels")
     // console.log(listCardLabels.classList)
     if (cardLabels = listCardLabels.querySelector(".card-label")){
-        cardLabelColour = cardLabels.className;
-        console.log(cardLabelColour
-        )}
+        cardLabels.classList.forEach(cardLabel => {
+            // console.log(cardLabel)
+            if (cardLabel.includes("card-label-")) {
+                console.log(cardLabel.substr(11,))
+                card.style.backgroundColor = cardLabel.substr(11,);
+            }
+        })
+    }
         // card.style.backgroundColor = "#eb5a4647";
     })
