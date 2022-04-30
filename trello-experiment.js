0a1214d9241b256d2adfe43cb9487e4707e004fa
z <z@pingflag.com>
2022-04-30 13:53:17 +1000

    selected card class names

--- trello-experiment.js
+++ trello-experiment.js
@@ -1,4 +1,14 @@
 const rootStyle = document.getElementById("trello-root")
 rootStyle.classList.add("body-card-label-text")
 
+const listOfCards = document.querySelectorAll(".list-card.js-member-droppable.ui-droppable")
 
+listOfCards.forEach(card => {
+    listCardLabels = card.querySelector(".list-card-labels")
+    // console.log(listCardLabels.classList)
+    if (cardLabels = listCardLabels.querySelector(".card-label")){
+        cardLabelColour = cardLabels.className;
+        console.log(cardLabelColour
+        )}
+        // card.style.backgroundColor = "#eb5a4647";
+    })
