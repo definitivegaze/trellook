42968b3d9dbab1809e44746e9aa3b944883f6127
z <z@pingflag.com>
2022-05-01 13:12:44 +1000

    added colour map and changed background colour formatting

--- trello-experiment.js
+++ trello-experiment.js
@@ -6,6 +6,19 @@ rootStyle.classList.add("body-card-label-text")
 // get colours from card labels
 // filled card with colours
 
+const colourMapping = {
+    "green": "#bde3b5",
+    "red": "#ff9999",
+    "yellow": "#ffffb3",
+    "orange": "#ffff99",
+    "blue": "#99daff",
+    "purple": "#d9aaee",
+    "sky": "#99f1ff",
+    "pink": "#ff99d8",
+    "black": "#bcc8dc",
+    "lime": "#a5f3cd"
+}
+
 const listOfCards = document.querySelectorAll(".list-card.js-member-droppable.ui-droppable")
 
 function get_label_colours(card) {
@@ -23,18 +36,20 @@ function get_label_colours(card) {
                 listOfLabelColours.push(labelColour)};
             })
     }
-    console.log(listOfLabelColours);
+    // console.log(listOfLabelColours);
     return (card,listOfLabelColours);
 }
 
 function change_background_colour(card, listOfColours){
     if (listOfColours.length == 1){
-            card.style.backgroundColor = listOfColours[0];
+        card.style.backgroundColor = colourMapping[listOfColours[0]];
     }
     if (listOfColours.length > 1){
-    listOfColoursString = listOfColours.join(", ");
-    console.log(listOfColoursString);
-    card.style.backgroundImage = `linear-gradient(to right, ${listOfColoursString})`;
+        percentage = (1/(listOfColours.length)*100).toFixed(0)
+        listOfColoursJoin = listOfColours.map(colour => colourMapping[colour]).join(` ${percentage}%, `);
+        listOfColoursString = `linear-gradient(150deg, ${listOfColoursJoin} ${percentage}%)`;
+        console.log(listOfColoursString)
+        card.style.backgroundImage = listOfColoursString;
     }
 }
 
